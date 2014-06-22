6a809fb273acad2eac06f2ad257989654af55f04
Anton nout <webadmin87@gmail.com>
2014-06-22 22:54:12 +0400

    виджет вывода списка элементов

---
+++ app/modules/main/widgets/elist/EList.php
@@ -0,0 +1,103 @@
+<?php
+namespace app\modules\main\widgets\elist;
+
+use Yii;
+use common\widgets\App;
+
+/**
+ * Class EList
+ * Виджет для вывода элементов. Отображаемая сущность должна наследовать \common\db\ActiveRecord
+ * @package app\modules\main\widgets\treelist
+ * @author Churkin Anton <webadmin87@gmail.com>
+ */
+
+class EList extends App {
+
+    /**
+     * @var string имя класса модели
+     */
+    public $modelClass;
+
+    /**
+     * @var int количество выводимых элементов
+     */
+
+    public $limit = 3;
+
+    /**
+     * @var array сортировка элементов
+     */
+
+    public $order = ['id'=>'DESC'];
+
+    /**
+     * @var closure функция возвращающая url модели. Принимает аргументом модель для которой необходимо создать url
+     */
+    public $urlCreate;
+
+    /**
+     * @var closure функция для модификации запроса. Принимает аргументом \common\db\TActiveQuery
+     */
+    public $queryModify;
+
+    /**
+     * @var array массив атрибутов html тега
+     */
+
+    public $options = array();
+
+    /**
+     * @var array массив моделей
+     */
+    protected $models;
+
+    /**
+     * @var int глубина родительского раздела
+     */
+
+    protected $parentLevel;
+
+    /**
+     * @inheritdoc
+     */
+
+    public function init() {
+
+        if(!$this->isShow())
+            return false;
+
+        $class = $this->modelClass;
+
+        $query = $class::find()->published()->orderBy($this->order)->limit($this->limit);
+
+        if(is_callable($this->queryModify)) {
+
+            $func = $this->queryModify;
+
+            $func($query);
+
+        }
+
+        $this->models = $query->all();
+
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+
+    public function run() {
+
+        if(!$this->isShow() OR empty($this->models))
+            return false;
+
+        return $this->render("index",[
+            "models"=>$this->models,
+            "options"=>$this->options,
+            "urlCreate"=>$this->urlCreate,
+        ]);
+
+    }
+
+}
---
+++ app/modules/main/widgets/elist/views/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @var array $models массив моделей меню
+ * @var array $options массив html атрибутов корневого Ul
+ * @var closure $urlCreate функция для содания url
+ */
+
+use Yii;
+use yii\helpers\Html;
+
+echo Html::beginTag('ul', $options)."\n";
+
+foreach ($models AS $model) {
+
+    $link = "";
+
+    if(is_callable($urlCreate))
+        $link = $urlCreate($model);
+
+    echo Html::tag('li', Html::a( $model->title, $link));
+
+}
+
+echo Html::endTag('ul');
+
+
