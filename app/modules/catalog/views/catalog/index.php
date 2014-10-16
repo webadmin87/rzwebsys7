56bffa4176043d5ba55bb4b7aa8433d6f144f30a
Anton office <webadmin87@gmail.com>
2014-10-16 15:43:25 +0400

    улучшения в модуле каталога

--- app/modules/catalog/views/catalog/index.php
+++ app/modules/catalog/views/catalog/index.php
@@ -3,6 +3,7 @@
  * @var yii\web\View $this
  * @var string $html html код грида с товарами
  * @var \app\modules\catalog\models\CatalogSection|null $sectionModel модель категории
+ * @var \app\modules\catalog\models\Catalog $model модель элемента каталога
  */
 ?>
 <h1><?= $sectionModel ? $sectionModel->title : Yii::t('catalog/app', 'Catalog') ?></h1>
