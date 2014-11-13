0c5d87f6134a27be8a008190a9771eccb10f0ac2
Anton office <webadmin87@gmail.com>
2014-11-13 15:38:32 +0300

    настройка окружений

--- environments/dev/app/web/index.php
+++ environments/dev/app/web/index.php
@@ -1,4 +1,7 @@
 <?php
+
+error_reporting(E_ALL & ~E_STRICT);
+
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
--- environments/prod/app/web/index.php
+++ environments/prod/app/web/index.php
@@ -1,4 +1,7 @@
 <?php
+
+error_reporting(0);
+
 defined('YII_DEBUG') or define('YII_DEBUG', false);
 defined('YII_ENV') or define('YII_ENV', 'prod');
 
