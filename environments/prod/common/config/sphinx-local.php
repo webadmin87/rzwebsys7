94da4d6f76d8fe22fa38c83652896942019ff60c
Anton office <webadmin87@gmail.com>
2015-08-18 13:13:49 +0300

    улучшения

--- environments/prod/common/config/sphinx-local.php
+++ environments/prod/common/config/sphinx-local.php
@@ -4,7 +4,7 @@ return [
 	"type"	=> "pgsql",
 	"sql_host"	=> "localhost",
 	"sql_user"	=> "rzwebsys7",
-	"sql_pass"	=> "xh48u56",
+	"sql_pass"	=> "password",
 	"sql_db"	=> "rzwebsys7",
 	"sql_port"	=> 5432,
 
