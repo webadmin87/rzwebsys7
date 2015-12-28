f7adf7fb673200178647db78a31384fb21e3cb87
nanson <panopticum87@gmail.com>
2015-12-28 17:59:14 +0300

    user unique username and email constraints

--- app/modules/main/migrations/m151228_143046_alter_user_unique_constraint.php
+++ app/modules/main/migrations/m151228_143046_alter_user_unique_constraint.php
@@ -7,13 +7,13 @@ class m151228_143046_alter_user_unique_constraint extends \app\modules\main\db\M
 
     public function safeUp()
     {
-       $this->execute("ALTER TABLE \"$this->tableName\" ADD CONSTRAINT user_uniq_username_constraint UNIQUE (username);");
-       $this->execute("ALTER TABLE \"$this->tableName\" ADD CONSTRAINT user_uniq_email_constraint UNIQUE (email);");
+       $this->execute("ALTER TABLE \"$this->tableName\" ADD CONSTRAINT user_unique_username_constraint UNIQUE (username);");
+       $this->execute("ALTER TABLE \"$this->tableName\" ADD CONSTRAINT user_unique_email_constraint UNIQUE (email);");
     }
 
     public function safeDown()
     {
-	    $this->execute("ALTER TABLE \"$this->tableName\" DROP CONSTRAINT user_uniq_username_constraint UNIQUE (username);");
-	    $this->execute("ALTER TABLE \"$this->tableName\" DROP CONSTRAINT user_uniq_email_constraint UNIQUE (email);");
+	    $this->execute("ALTER TABLE \"$this->tableName\" DROP CONSTRAINT user_unique_username_constraint;");
+	    $this->execute("ALTER TABLE \"$this->tableName\" DROP CONSTRAINT user_unique_email_constraint;");
     }
 }
